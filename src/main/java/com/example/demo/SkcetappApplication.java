686afa0fb01a5b75c9c20c6c40a8d7df343893b3
SIBIRAJST <22csd040@skcet.ac.in>
2024-05-04 17:31:25 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SkcetappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkcetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkcetappApplication.class, args);
+	}
+
+}
